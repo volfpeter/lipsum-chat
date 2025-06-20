6291c0e9de912613da764919a60f008a5caee30a
Peter Volf <do.volfp@gmail.com>
2025-06-20 18:02:08 +0200

    docs: added some more docs for the copy_button() component

--- lc/ui/copy_button.py
+++ lc/ui/copy_button.py
@@ -7,6 +7,8 @@ def copy_button() -> Snippet:
     """
     Copy button.
 
-    It must be within an AlpineJS component that has a `copy()` action and a `copied` state.
+    It must be within an AlpineJS component that has a `copy()` action and a `copied` state,
+    and it attempts to copy the text content of the element (within the AlpineJS component)
+    that has the `x_ref="clipboardContent"` attribute.
     """
     return Snippet(replace_py_extension(__file__))
